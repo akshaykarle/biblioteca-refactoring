9614b70e531237abf7db2f487897855f999ad0fe
Akshay Karle <akshay.a.karle@gmail.com>
2012-09-25 12:22:54 +0530

    Pull out methods from main.

--- src/main/Program.java
+++ src/main/Program.java
@@ -9,108 +9,23 @@ public class Program {
 
     public static void main(String[] args) {
         while (true) {
-            System.out.println("**********************************************************");
-            System.out.println("* Welcome to The Bangalore Public Library System - Biblioteca *");
-            System.out.println("**********************************************************");
-            System.out.println("*                Menu                                    *");
-            System.out.println("*         =====================                          *");
-            System.out.println("*         1. List Book Catalog                           *");
-            System.out.println("*         2. Check out Book                              *");
-            System.out.println("*         3. Check Library Number                        *");
-            System.out.println("*         4. Movie Listing                               *");
-            System.out.println("*         5. Login                                       *");
-            System.out.println("*         9. Exit                                        *");
-            System.out.println("**********************************************************");
-            System.out.println("Your Selection: ");
+            displayMenu();
 
             InputStreamReader inputStream = new InputStreamReader(System.in);
             BufferedReader reader = new BufferedReader(inputStream);
             int i1 = 0;
-            try {
-                String value = reader.readLine();
-                i1 = Integer.parseInt(value);
-            } catch (Exception e) {
-                // Do you know what numbers are!!!
-                System.out.println("Enter a valid integer!!");
-            }
+            i1 = getUserInputNumber(reader, i1);
 
             if (i1 == 1) {
-                System.out.println(" 1. Sweet Valley High vol. 4 by John Travolta ");
-                System.out.println(" 2. eXtreme Programming Explained by Kent Beck ");
-                System.out.println(" 3. How to Win Friends and Influence People by Dale Carnagie ");
-                System.out.println(" 4. How to Cheat at TWU Assignements by Anonymous ");
+                displayBooks();
             } else if (i1 == 2) {
-                System.out.println(" Please enter the number of the book you wish to checkout: ");
-                int i2 = 0;
-                try {
-                    i2 = Integer.parseInt(reader.readLine());
-                } catch (Exception e) {
-                    // Do you know what numbers are!!!
-                    System.out.println("Enter a valid integer!!");
-
-                }
-                switch (i2) {
-                    case 1:
-                        System.out.println("\n");
-                        System.out.println(" Thank You! Enjoy the book.");
-                        break;
-                    case 2:
-                        System.out.println("\n");
-                        System.out.println(" Thank You! Enjoy the book.");
-                        break;
-                    case 3:
-                        System.out.println("\n");
-                        System.out.println(" Thank You! Enjoy the book.");
-                        break;
-                    case 4:
-                        System.out.println("\n");
-                        System.out.println(" Thank You! Enjoy the book.");
-                        break;
-                    default:
-                        System.out.println("\n");
-                        System.out.println("Sorry we don't have that book yet.");
-                }
+                checkOutBook(reader);
             } else if (i1 == 3) {
-                if (loggedIn()) {
-                    System.out.println("\n");
-                    System.out.println("Your library number is " + savedLibraryNumber);
-                } else {
-
-                    System.out.println("\n");
-                    System.out.println("Please talk to Librarian. Thank you.");
-                }
+                displayLibraryNumber();
             } else if (i1 == 4) {
-                System.out.println(createMovie("Rocky", "John G. Avildsen", "10"));
-                System.out.println(createMovie("Rocky II", "John G. Avildsen", "9"));
-                System.out.println(createMovie("Rocky III", "John G. Avildsen", "8"));
-                System.out.println(createMovie("Rocky IV", "John G. Avildsen", "7"));
-                System.out.println(createMovie("Rocky V", "John G. Avildsen", "8"));
-                System.out.println(createMovie("Drainage", "Francisco Trindade", "N/A"));
-                System.out.println(createMovie("The Shawshank Redemption", "Frank Darabont", "10"));
-                System.out.println(createMovie("The Godfather", "Francis Ford Coppola", "7"));
-                System.out.println(createMovie("Inception", "Frank Darabont", "10"));
-                System.out.println(createMovie("Pulp Fiction", "Quentin Tarantino", "6"));
+                displayMovies();
             } else if (i1 == 5) {
-                clearLogin();
-                System.out.println("Enter your library number");
-                try {
-                    String libraryNumber = reader.readLine();
-                    if (validLibraryNumber(libraryNumber)) {
-                        try {
-                            System.out.println("Enter your Password: ");
-                            String password = reader.readLine();
-                            if (validPassword(password)) {
-                                loggedIn = true;
-                                savedLibraryNumber = libraryNumber;
-                            }
-                        } catch (Exception e) {
-
-                        }
-                    }
-                } catch (Exception e) {
-
-                }
-
+                performLogin(reader);
             } else if (i1 == 9) {
                 System.out.println("Quitting...");
                 break;
@@ -119,6 +34,113 @@ public class Program {
                 System.out.println("Enter a valid integer!!");
             }
         }
+    }
+
+    private static void performLogin(BufferedReader reader) {
+        clearLogin();
+        System.out.println("Enter your library number");
+        try {
+            String libraryNumber = reader.readLine();
+            if (validLibraryNumber(libraryNumber)) {
+                try {
+                    System.out.println("Enter your Password: ");
+                    String password = reader.readLine();
+                    if (validPassword(password)) {
+                        loggedIn = true;
+                        savedLibraryNumber = libraryNumber;
+                    }
+                } catch (Exception e) {
+
+                }
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
+    private static void displayMovies() {
+        System.out.println(createMovie("Rocky", "John G. Avildsen", "10"));
+        System.out.println(createMovie("Rocky II", "John G. Avildsen", "9"));
+        System.out.println(createMovie("Rocky III", "John G. Avildsen", "8"));
+        System.out.println(createMovie("Rocky IV", "John G. Avildsen", "7"));
+        System.out.println(createMovie("Rocky V", "John G. Avildsen", "8"));
+        System.out.println(createMovie("Drainage", "Francisco Trindade", "N/A"));
+        System.out.println(createMovie("The Shawshank Redemption", "Frank Darabont", "10"));
+        System.out.println(createMovie("The Godfather", "Francis Ford Coppola", "7"));
+        System.out.println(createMovie("Inception", "Frank Darabont", "10"));
+        System.out.println(createMovie("Pulp Fiction", "Quentin Tarantino", "6"));
+    }
+
+    private static void displayLibraryNumber() {
+        if (loggedIn()) {
+            System.out.println("\n");
+            System.out.println("Your library number is " + savedLibraryNumber);
+        } else {
+
+            System.out.println("\n");
+            System.out.println("Please talk to Librarian. Thank you.");
+        }
+    }
+
+    private static void checkOutBook(BufferedReader reader) {
+        System.out.println(" Please enter the number of the book you wish to checkout: ");
+        int i2 = 0;
+        i2 = getUserInputNumber(reader, i2);
+        switch (i2) {
+            case 1:
+                System.out.println("\n");
+                System.out.println(" Thank You! Enjoy the book.");
+                break;
+            case 2:
+                System.out.println("\n");
+                System.out.println(" Thank You! Enjoy the book.");
+                break;
+            case 3:
+                System.out.println("\n");
+                System.out.println(" Thank You! Enjoy the book.");
+                break;
+            case 4:
+                System.out.println("\n");
+                System.out.println(" Thank You! Enjoy the book.");
+                break;
+            default:
+                System.out.println("\n");
+                System.out.println("Sorry we don't have that book yet.");
+        }
+    }
+
+    private static void displayBooks() {
+        System.out.println(" 1. Sweet Valley High vol. 4 by John Travolta ");
+        System.out.println(" 2. eXtreme Programming Explained by Kent Beck ");
+        System.out.println(" 3. How to Win Friends and Influence People by Dale Carnagie ");
+        System.out.println(" 4. How to Cheat at TWU Assignements by Anonymous ");
+    }
+
+    private static int getUserInputNumber(BufferedReader reader, int i1) {
+        try {
+            String value = reader.readLine();
+            i1 = Integer.parseInt(value);
+        } catch (Exception e) {
+            // Do you know what numbers are!!!
+            System.out.println("Enter a valid integer!!");
+        }
+        return i1;
+    }
+
+    private static void displayMenu() {
+        System.out.println("**********************************************************");
+        System.out.println("* Welcome to The Bangalore Public Library System - Biblioteca *");
+        System.out.println("**********************************************************");
+        System.out.println("*                Menu                                    *");
+        System.out.println("*         =====================                          *");
+        System.out.println("*         1. List Book Catalog                           *");
+        System.out.println("*         2. Check out Book                              *");
+        System.out.println("*         3. Check Library Number                        *");
+        System.out.println("*         4. Movie Listing                               *");
+        System.out.println("*         5. Login                                       *");
+        System.out.println("*         9. Exit                                        *");
+        System.out.println("**********************************************************");
+        System.out.println("Your Selection: ");
     }
 
     private static boolean validPassword(String password) {
